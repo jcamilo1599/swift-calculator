2e6a576bcbe77d5afcd8cdcabbc9a7d44ea2a87d
Juan Camilo <99774186+jcamilo1599@users.noreply.github.com>
2023-05-12 22:51:34 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Juan Camilo Marín Ochoa on 12/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
